4593a5f786548f9f6d37725126179ae4a153bf46
Gergo Horvath <111783205+horger89@users.noreply.github.com>
2024-04-07 22:02:57 +0100

    Fixed Linting

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,6 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     # Define the admin pages for users
     ordering = ['id']
@@ -39,8 +40,6 @@ class UserAdmin(BaseUserAdmin):
             ),
         }),
     )
-
-
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     # Manager for users
 
@@ -19,7 +20,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         # Create and return a new superuser
         user = self.create_user(email, password)
@@ -29,7 +30,6 @@ class UserManager(BaseUserManager):
 
         return user
 
- 
 
 class User(AbstractBaseUser, PermissionsMixin):
     # User in the system
@@ -42,4 +42,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    
